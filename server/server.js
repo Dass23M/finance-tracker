b2f8d426990a61ec149bedf8816d316e263e340e
Dass23M <157539720+Dass23M@users.noreply.github.com>
2026-05-11 14:28:30 +0530

    add api routes in server.js

--- server/server.js
+++ server/server.js
@@ -49,7 +49,7 @@ app.get('/api/health', (req, res) => {
 });
 
 // API Routes — will add these in the next steps
-// app.use('/api/auth', require('./src/routes/auth'));
+ app.use('/api/auth', require('./src/routes/auth'));
 // app.use('/api/transactions', require('./src/routes/transactions'));
 // app.use('/api/budgets', require('./src/routes/budgets'));
 // app.use('/api/categories', require('./src/routes/categories'));
